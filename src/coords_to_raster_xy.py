82de7270b0d5ade5d4038cd3b425b62f331a6141
bendmorris <ben@bendmorris.com>
2013-10-08 11:14:13 -0400

    Adding distance function; refactoring functions to always use (y,x) points for consistency.

--- src/coords_to_raster_xy.py
+++ src/coords_to_raster_xy.py
@@ -1,29 +1,31 @@
 import doctest
 
 
-def xy_coords(lat, lon, ul_x, ul_y, x_dim, y_dim, x_size, y_size):
+def xy_coords(point, ul, dims, sizes=None):
     '''Given a latitude/longitude pair and resolution information about the 
     raster file, gives the row/column of the corresponding raster matrix.
 
-    >>> xy_coords(0, 0, 0, 0, 10, 10, 100, 100)
+    >>> xy_coords((0, 0), (0, 0), (10, 10), (100, 100))
     (0, 0)
-    >>> xy_coords(0, 10, 0, 0, 10, 10, 100, 100)
+    >>> xy_coords((0, 10), (0, 0), (10, 10), (100, 100))
     (0, 1)
-    >>> xy_coords(-10, 0, 0, 0, 10, 10, 100, 100)
+    >>> xy_coords((-10, 0), (0, 0), (10, 10), (100, 100))
     (1, 0)
-    >>> xy_coords(-2, 0, 0, 0, 2.5, 0.5, 100, 100)
+    >>> xy_coords((-2, 0), (0, 0), (0.5, 2.5), (100, 100))
     (4, 0)
-    >>> xy_coords(-5, 0, 0, 0, 1, 1, 4, 4)
+    >>> xy_coords((-5, 0), (0, 0), (1, 1), (4, 4))
     (1, 0)
-    >>> xy_coords(0, 180, 0, 0, 1, 1, 360, 180) == xy_coords(0, -180, 0, 0, 1, 1, 360, 180)
+    >>> xy_coords((0, 180), (0, 0), (1, 1), (180, 360)) == xy_coords((0, -180), (0, 0), (1, 1), (180, 360))
     True
     '''
-
-    dy = int(round((ul_y-lat)/y_dim)) % y_size
-    dx = int(round((lon-ul_x)/x_dim)) % x_size
+    
+    # point and ul are (lat, lon) tuples;
+    # positive y-direction in raster vs on globe are reversed
+    # (positive is North, but down in raster)
+    dy = int(round((ul[0]-point[0])/dims[0]))
+    dx = int(round((point[1]-ul[1])/dims[1]))
+    if not sizes is None:
+        dy %= sizes[0]
+        dx %= sizes[1]
 
     return dy, dx
-
-
-if __name__ == '__main__':
-    doctest.testmod()
--- src/get_data.py
+++ src/get_data.py
@@ -1,4 +1,5 @@
 import os
+import math
 import gdal
 from read_headers import variable_names
 from coords_to_raster_xy import xy_coords
@@ -23,7 +24,7 @@ def get_dataset(file):
 def dim(width, size):
     return float(width)/size
 
-def get_values(file, points, ul_x=ul[1], ul_y=ul[0], lr_x=lr[1], lr_y=lr[0]):
+def get_values(file, points, ul=ul, lr=lr):
     '''Given a .bil file (or other file readable by GDAL) and a set of (lat,lon) 
     points, return a list of values for those points. -9999 will be converted to 
     None.
@@ -36,11 +37,52 @@ def get_values(file, points, ul_x=ul[1], ul_y=ul[0], lr_x=lr[1], lr_y=lr[0]):
     data = get_dataset(file)
     raster = data.ReadAsArray()
     
-    xsize, ysize = data.RasterXSize, data.RasterYSize
-    xdim = dim(lr_x-ul_x, xsize)
-    ydim = dim(ul_y-lr_y, ysize)
+    size = data.RasterYSize, data.RasterXSize
+    xdim = dim(lr[1]-ul[1], size[1])
+    ydim = dim(ul[0]-lr[0], size[0])
+    dims = (ydim, xdim)
 
-    result = [raster[xy_coords(lat, lon, ul_x, ul_y, xdim, ydim, xsize, ysize)] for (lat, lon) in points]
+    result = [raster[xy_coords((lat, lon), ul, dims, size)] for (lat, lon) in points]
     result = [None if value == -9999 else value for value in result]
 
     return result
+
+def radians(deg):
+    '''Converts degrees to radians.
+
+    >>> abs(radians(0)) < 0.00001
+    True
+    >>> abs(radians(90) - math.pi/2) < 0.00001
+    True
+    >>> abs(radians(180) - math.pi) < 0.00001
+    True
+    >>> abs(radians(270) - 3*math.pi/2) < 0.00001
+    True
+    >>> abs(radians(360) - 2*math.pi) < 0.00001
+    True
+    '''
+    return deg/180.*(math.pi)
+
+def distance(p1, p2):
+    '''Approximate distance between two lat/lon points, in km.
+    
+    p1 and p2 should be tuples containing (lat,lon)
+    
+    >>> 1250 <= distance((50, -50), (40, -40)) <= 1500
+    True
+    >>> 1000 <= distance((50, -50), (40, -50)) <= 1250
+    True
+    '''
+    y1, x1 = p1
+    y2, x2 = p2
+    dy = radians(abs(y2-y1))
+    dx = radians(abs(x2-x1))
+    a = math.sin(dy/2)**2 + math.sin(dx/2)**2 * math.cos(radians(y1)) * math.cos(radians(y2))
+    c = 2 * math.atan2(a**0.5, (1-a)**0.5)
+    return 6371 * c
+
+def points_within_distance(start_point, radius, ul, dims):
+    '''Find the set of lat/lon coords in a raster that are within `radius` km of 
+    the starting point.'''
+    pass
+    
